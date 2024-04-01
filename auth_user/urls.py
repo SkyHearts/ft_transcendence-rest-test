e609a59eb6fe79eeb645bbc31cfea5f5d1f5e0bf
mseong123 <mseong123@hotmail.com>
2024-04-01 10:07:49 +0800

    Revert "merge with front/game"
    This reverts commit b8817de9acc0361c0fc7d3f8e7661c405d236b8b, reversing
    changes made to 9dd7500825422bdf1f234732567b1d4ef32bec79.

--- auth_user/urls.py
+++ auth_user/urls.py
@@ -7,6 +7,7 @@ app_name = "auth_user"
 urlpatterns = [
     path("send_otp/", views.send_otp, name="send_otp"),
     path("login/", views.login_with_otp, name="login"),
-    path("get_username/", views.get_username, name="user"),
+    path("register/", views.register, name="register"),
+    path("get_user/", views.get_user, name="user"),
 	path("session_auth/", views.session_auth, name="session_auth"),
 ]
